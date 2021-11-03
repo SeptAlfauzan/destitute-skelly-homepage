3e0e62d1ae0aea2333923f4951da436ead9b0f23
SeptAlfauzan <alfauzansepta@gmail.com>
2021-11-03 10:00:49 +0700

    fix typo

--- components/progress-container.js
+++ components/progress-container.js
@@ -5,7 +5,7 @@ export default function ProgressContainer() {
         <div id="progress" className="flex   flex-wrap min-h-screen w-full mt-28 md:mt-0 mb-28 content-center">
         <div className="relative w-full z-10 -bottom-10">
           <h3 className="text-4xl w-3/4 md:mx-auto md:text-6xl text-left ml-20">
-            Our Work Weekly Progress
+            Our Weekly Progress
           </h3>
         </div>
         <div className="relative w-full bg-primary-dark py-24">
