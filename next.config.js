f4b0487263259b8b8ee3f72867346f530847dec2
SeptAlfauzan <alfauzansepta@gmail.com>
2021-11-29 10:07:10 +0700

    change next.config.js for fs module config

--- next.config.js
+++ next.config.js
@@ -1,1 +1,13 @@
-module.exports = {build: { extend(config, {}) { config.node = { fs: 'empty' } }}}
+module.exports = {future: {
+    webpack5: true, // by default, if you customize webpack config, they switch back to version 4. 
+      // Looks like backward compatibility approach.
+  },
+  webpack(config) {
+    config.resolve.fallback = {
+      ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
+        // by next.js will be dropped. Doesn't make much sense, but how it is
+      fs: false, // the solution
+    };
+
+    return config;
+  },}
